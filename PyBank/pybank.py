ea87207cdea280e47e8cf1b4bc903d5f85aaf235
Cory Potter <pottercory22@gmail.com>
2020-08-18 12:53:25 -0500

    Got it to work in the terminal

--- PyBank/pybank.py
+++ PyBank/pybank.py
@@ -8,9 +8,9 @@ net_amount = 0
 change_amounts = []
 greatest_inc = 0
 greatest_dec = 0
-# Use change_amounts list to calculate average change
-avg_change = sum(change_amounts) / len(change_amounts)
-
+profit_loss_change = 0
+# we have to set previous_profit_loss to 0 for the first line when calculating profit/loss change
+previous_profit_loss = 0
     
 # Path to collect data from the Resources folder
 pybank_csv = os.path.join("Resources", "budget_data.csv")
@@ -25,15 +25,35 @@ with open (pybank_csv) as csvfile:
         # for each row we will add one to the total_months variable
         total_months += 1
         # for each row we are adding the Profit/losses amount to the net_amount variable
-        net_amount += row[1]
-        for row + 1 in csvreader:
-            change_amounts.append((row + 1) - row)
+        net_amount += int(row[1])
+        # calculate profit/loss change
+        profit_loss_change = int(row[1]) - previous_profit_loss
+        # set previous_profit_loss to the current profit/losses to help us calculate the change for the next row
+        previous_profit_loss = int(row[1])        
+        # add each profit/loss change amount to the change amounts list
+        change_amounts.append(profit_loss_change)
+        # use an if statement to find greatest increase and decrease
+        if profit_loss_change > greatest_inc:
+            greatest_inc = profit_loss_change
+            # create variable for greatest increase month
+            increase_month = row[0]
+        elif profit_loss_change < greatest_dec:
+            greatest_dec = profit_loss_change
+            # create variable for greatest decrease month 
+            decrease_month = row[0]
+        # if the profit/loss change is not > greatest increase and not < greatest decrease, we want to continue to the next row
+        else:
+            continue
+    # Use change_amounts list to calculate average change
+    avg_change = sum(change_amounts) / len(change_amounts)
+
+        
         
 print("Financial Analysis")
 print("---------------------------")
 print(f"Total Months: {total_months}")
 print(f"Total: {net_amount}")
-print(f"Average Change: ${avg_change}")
-print(f"Greatest Increase in Profits: (${greatest_inc})")
-print(f"Greatest Decrease in Profits: (${greatest_dec})")
+print(f"Average Change: ${round(avg_change,2)}")
+print(f"Greatest Increase in Profits: {increase_month} (${greatest_inc})")
+print(f"Greatest Decrease in Profits: {decrease_month} (${greatest_dec})")
 
